369a89f5c62bd36a78757daccaf4c3dcb8ce4023
letiziamoda22 <letiziamoda22@gmail.com>
2026-07-05 17:14:41 +0200

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
